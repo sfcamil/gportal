63170c2bd9f0cdc75deb5e41d900990db7189a06
sfcamil <sfcamil@yahoo.fr>
2022-11-03 12:30:09 +0100

    modif and create autocomplete for change adh actif assistante

--- js/adh-actif-autocomplete.js
+++ js/adh-actif-autocomplete.js
@@ -1,17 +1,25 @@
-(function ($, Drupal, drupalSettings) {
+(function ($, Drupal) {
     'use strict';
     Drupal.behaviors.replaceAutocomplete = {
         attach: function (context, settings) {
             // debugger;
             console.clear();
+            var intialValue = $('.replaceAutocomplete').val();
+            console.log("1: " + intialValue);
 
             $('.replaceAutocomplete').click(function () {
                 this.value = '';
             });
 
+            $('.replaceAutocomplete').blur(function () {
+                $('.replaceAutocomplete').val(intialValue);
+                console.log("2: " +intialValue);
+            });
+
             // https://stackoverflow.com/questions/42226161/input-field-remove-value-when-onclick-events-and-restore-the-value-if-not-set
             // attach custom select handler to fields with class
             $('.replaceAutocomplete').autocomplete({
+                autoFocus: true,
                 minLength: 2, /* nombre de caractères minimaux pour lancer une recherche */
                 delay: 200, /* delais après la dernière touche appuyée avant de lancer une recherche */
                 scrollHeight: 320,
@@ -21,7 +29,8 @@
                         var valueFieldName = event.target.name + '_value';
                         if ($('input[name=' + valueFieldName + ']').length > 0) {
                             valueField = $('input[name=' + valueFieldName + ']');
-                            console.log(valueField);
+                            var intialValue = valueField;
+                            console.log("3: " + valueField);
                             // update the labels too
                             const labels = Drupal.autocomplete.splitValues(event.target.value);
                             labels.pop();
@@ -34,12 +43,13 @@
                     terms.pop();
                     // Add the selected item.
                     terms.push(ui.item.value);
-
                     valueField.val(terms.join(', '));
+                    // submit
+                    $(this).closest('form').submit();
                     // Return false to tell jQuery UI that we've filled in the value already.
                     return false;
                 }
             });
         }
     };
-})(jQuery, Drupal, drupalSettings);
+})(jQuery, Drupal);
